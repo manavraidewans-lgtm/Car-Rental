d52fe17b7938d2d3d0f83efa64567667f57705aa
Manav Rai Dewan <manavraidewans@Gmail.com>
2026-07-25 12:35:28 +0530

    navbar done

---
+++ logic.js
@@ -0,0 +1,17 @@
+// Hamburger //
+
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".navbar-links");
+const navItems = document.querySelectorAll(".navbar-links ul li a");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navLinks.classList.toggle("active");
+});
+
+navItems.forEach((link) => {
+    link.addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        navLinks.classList.remove("active");
+    });
+});
